5d922cf0f31a1dffac08dd8f121613b646619731
shivam <shivam.singh@autodap.in>
2024-05-25 13:02:13 +0530

    first commit

---
+++ readme.md
@@ -0,0 +1,2 @@
+docker build -t my-express-app .
+docker run -p 3000:3000 my-express-app
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+console.log("process.env.testENV => ", process.env.testENV);
+
+// Set the port the server will listen on
+const PORT = process.env.PORT || 3000;
+
+// Define a simple GET route
+app.get('/env', (req, res) => {
+  console.log(process.env);
+  let resData = {
+    port: PORT,
+    testENV: process.env.testENV,
+  }
+
+  console.log("resData => ", resData);
+
+  res.send(resData)
+});
+
+
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
