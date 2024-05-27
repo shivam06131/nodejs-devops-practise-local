329a87e8020740ea12fbe10959567043995b5c20
shivam <shivam.singh@autodap.in>
2024-05-27 16:36:46 +0530

    test 01

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 console.log("process.env.testENV => ", process.env.TESTENV);
 
 // Set the port the server will listen on
-let PORT = process.env.PORT || 3000;
+let PORT = 4000;
 
 // Define a simple GET route
 app.get('/env', (req, res) => {
